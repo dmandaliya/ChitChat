1d6607ed9025e0772d3790b6be86f5bb41df813e
Deep <mandalid@uwindsor.ca>
2026-02-27 20:02:46 -0500

    Sprint 1 Complete: Multi-module Maven architecture (parent, shared, server, client)

---
+++ shared/src/main/java/com/chitchat/shared/MessageType.java
@@ -0,0 +1,12 @@
+package com.chitchat.shared;
+
+public enum MessageType {
+    LOGIN,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    PUBLIC_MESSAGE,
+    PRIVATE_MESSAGE,
+    USER_LIST,
+    LOGOUT,
+    ERROR
+}
